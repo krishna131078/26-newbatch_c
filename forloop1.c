5bc25a162a80f9f11b44a401657d6a83e0005700
krishna131078 <krishna131078@gmail.com>
2026-01-26 18:40:33 +0530

    loops

---
+++ forloop1.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+// init
+//conditon
+// incre/decre
+// ++ --
+// pre/post
+
+// a=10
+// ++---->incre/print
+// post---->print/incre
+int main()
+{
+    int a=50;
+    // printf("%d",a);
+    // // a=20;
+    // printf("\n%d",++a);
+    
+    // printf("\n%d",a);
+    // printf("\n%d",a++);
+    // printf("\n%d",a);
+    // printf("\n%d",++a);
+    // printf("\n%d",a--);
+    // printf("\n%d",a);
+    // printf("\n%d",--a);
+    // printf("\n%d",a--);
+    // printf("\n%d",a--);
+    printf("%d",++a);
+    printf("\n%d",++a);
+    printf("\n%d",a);
+    printf("\n%d",a++);
+    // printf("\n%d",a);
+    printf("\n%d",++a);
+    printf("\n%d",--a);
+    printf("\n%d",a);
+    printf("\n%d",a++);
+    printf("\n%d",a--);
+    printf("\n%d",a);
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
---
+++ forloop2.c
@@ -0,0 +1,16 @@
+// i=1--->init
+// i<=10--->condition
+// i++incre/decre   
+
+#include<stdio.h>
+
+int main()
+{
+    int i,n;
+    printf("enter the num");
+    scanf("%d",&n);
+    for(i=1;i<=n;i+=3)
+    {
+        printf("%d\n",i);//1//2
+    }
+}
